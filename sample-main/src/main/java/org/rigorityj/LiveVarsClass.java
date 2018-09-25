9534fc3650c848fcb3107b64e0521ffadad33e6a
RigorityJ <rigority@gmail.com>
2018-09-24 20:49:21 -0400

    Add sample source

---
+++ sample-main/src/main/java/org/rigorityj/LiveVarsClass.java
@@ -0,0 +1,8 @@
+package org.rigorityj;
+
+import rigorityj.AESEncryptor;
+
+public class LiveVarsClass {
+    private static String xxx1 = VeryBusyClass.xxx;
+    private static final AESEncryptor aes = new AESEncryptor(xxx1);
+}
---
+++ sample-util/src/main/java/rigorityj/AESEncryptorBase.java
@@ -0,0 +1,10 @@
+package rigorityj;
+
+public class AESEncryptorBase {
+
+    private String str;
+
+    public AESEncryptorBase(String passPhrase) {
+        str = passPhrase;
+    }
+}
