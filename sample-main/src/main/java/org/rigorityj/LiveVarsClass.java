e79f1db413a4d3af492fbb4f9c3ae8dd5df7b9ba
RigorityJ <rigority@gmail.com>
2018-09-27 20:48:26 -0400

    Update source

--- sample-main/src/main/java/org/rigorityj/LiveVarsClass.java
+++
@@ -1,8 +0,0 @@
-package org.rigorityj;
-
-import rigorityj.AESEncryptor;
-
-public class LiveVarsClass {
-    private static String xxx1 = VeryBusyClass.xxx;
-    private static final AESEncryptor aes = new AESEncryptor(xxx1);
-}
